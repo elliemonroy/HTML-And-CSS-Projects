d487c4e88e229ecb01343b1fe3ef0425a373caa3
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-01-18 12:16:19 -0800

    This is my Basic_HTML_and_JavaScript_1 folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Hi, this is an alert!");
+
+
+function My_First_Function() {
+	    var String = "Kiss me, I'm Irish!";
+	    var result = String.fontcolor("green");
+	    document.getElementById("Irish").innerHTML =
+	    result;
+	}
